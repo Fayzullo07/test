1b35a99243cdf40b0630055b5eb987f45c742648
Fayzullo07 <jorayevfayzillo77@gmail.com>
2023-08-20 13:27:41 +0500

    api test 3

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const app = express()
-require("dotenv")
-const port = process.env.PORT | 3000
+require("dotenv").config();
+const { env } = process;
+
+app.use(express.json())
+
+const port = env.PORT | 5000;
 
 app.get('/', (req, res) => {
     res.json({ api: 'Hello World!' })
